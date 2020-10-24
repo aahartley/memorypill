d5cd134b87fc75035f34a4adc68a164db2d7fb4a
Austin Hartley <50050814+aahartley@users.noreply.github.com>
2020-10-23 21:57:36 -0400

    published

--- app/src/main/java/com/hartdroid/myapplication/MainActivity.java
+++ app/src/main/java/com/hartdroid/myapplication/MainActivity.java
@@ -1,14 +1,23 @@
-package com.example.myapplication;
+package com.hartdroid.myapplication;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 
 public class MainActivity extends AppCompatActivity {
     private final static String PREFS_KEY = "shared_prefs";
@@ -20,17 +29,64 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences prefs;
 
 
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                Log.d("MobileAds", "#onInitializationComplete");
+
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Log.d("mAdView", "onAdFailedToLoad. But why? "+errorCode);
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
+
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         prefs = getSharedPreferences(PREFS_KEY, Context.MODE_PRIVATE);
 
 
-        notificaitons();
 
 
         boolean first = prefs.getBoolean("first", true);
@@ -44,9 +100,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
+        start();
         }
-   // start();
 
     public void start(){
             final Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
@@ -116,10 +171,7 @@ public class MainActivity extends AppCompatActivity {
             }
     }
 
-    public void notificaitons(){
 
-
-    }
 
 
 
